0b8c034f43c916c1eb7ab2e600f78792c5353778
DhitGT <adityadwisaputra666@gmail.com>
2024-05-14 17:20:32 +0700

    push anjay

---
+++ pages/auth/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="min-height: 90vh">
+    <div
+      class="container d-flex flex-col items-center justify-center"
+      style="min-height: 90vh"
+    >
+      <div class="form bg-gray-800 pa-3 rounded-lg w-1/3">
+        <v-text-field label="Username" class="ma-0"> </v-text-field>
+        <v-text-field
+          label="Password"
+          type="password"
+          class="ma-0"
+          append-icon="mdi-eye"
+        >
+        </v-text-field>
+        <v-btn class="w-full"> LOGIN </v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
